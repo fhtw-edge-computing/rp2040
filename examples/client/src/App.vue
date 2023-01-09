7f64c55467e564608ccf57adac0f80b73ae443ef
Alija Sabic <sabic.alija@gmail.com>
2023-01-09 22:13:13 +0100

    Add simple rp2040 websocket client example with vue.js

---
+++ examples/client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>WebSocket API</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  created() {
+    const ws = new WebSocket("ws://10.4.0.31:8000");
+    // const ws = new WebSocket("ws://192.168.57.234:8000");
+    ws.onopen = function (/* event */) {
+      ws.send("Hello, server!");
+    };
+    ws.onmessage = function (event) {
+      console.log(event.data);
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
