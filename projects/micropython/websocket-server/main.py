aa8ece876e237c07ea945d392e793ffa365656d9
Alija Sabic <sabic.alija@gmail.com>
2023-01-15 19:59:17 +0100

    Update example websocket-server: change bind IP

--- projects/micropython/websocket-server/main.py
+++ projects/micropython/websocket-server/main.py
@@ -11,6 +11,6 @@ argv.append("<<pwd>>")
 async def main():
     if len(argv) == 2:
         connect(argv[0], argv[1])
-        await run_websocket_server("127.0.0.1", 8000)
+        await run_websocket_server("0.0.0.0", 8000)
 
 run(main())
