304dbc4198229cead6dfde8ea5a4859aa83af868
Alija Sabic <sabic.alija@gmail.com>
2023-01-17 00:28:19 +0100

    Update documentation: use medium-zoom for content images

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,18 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 import Layout from "./components/Layout.vue";
+import { onMounted } from "vue";
+import mediumZoom from "medium-zoom";
 
 import "./custom.css";
 
 export default {
   ...DefaultTheme,
   Layout: Layout,
+  setup() {
+    onMounted(() => {
+      // mediumZoom("[data-zoomable]", { background: "var(--vp-c-bg)" });
+      mediumZoom(".VPDoc img", { background: "var(--vp-c-bg)" });
+    });
+  },
 };
