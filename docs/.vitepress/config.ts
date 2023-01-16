e9c9605f3ce80bed80edfe8d84efed84b62b1616
Alija Sabic <sabic.alija@gmail.com>
2023-01-16 17:02:40 +0100

    Update documentation: disable searchbox temporarily

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,11 +18,12 @@ export default defineConfig({
   markdown: markdown(),
 
   themeConfig: {
-    algolia: {
-      appId: '2HWG3F9Q0X',
-      apiKey: '8a0075d717792b877e53c2bfee9871b7',
-      indexName: 'rp2040'
-    },
+    // algolia: {
+    //   appId: '2HWG3F9Q0X',
+    //   apiKey: '8a0075d717792b877e53c2bfee9871b7',
+    //   indexName: 'rp2040',
+    //   disableUserPersonalization: true
+    // },
     logo: "/img/fhtw-logo.svg",
     siteTitle: "Arduino RP2040",
     nav: nav(),
