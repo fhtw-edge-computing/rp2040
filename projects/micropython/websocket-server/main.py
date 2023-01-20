33b1c0a99e690bc8b8e60606315feb5cec38400d
Alija Sabic <sabic.alija@gmail.com>
2023-01-20 15:22:28 +0100

    Update rp2040 websocket server: refactor websocket library: allow handler registration for onConnect and onMessage hooks for individual servers, create opcode constants, refactor response, key generation and handshake

--- projects/micropython/websocket-server/main.py
+++ projects/micropython/websocket-server/main.py
@@ -2,15 +2,34 @@ from usys import argv
 from uasyncio import run
 
 from wifi import connect
-from websocket import run_websocket_server
+from websocket import run_websocket_server, send_websocket_frame, OPCODE_TEXT, OPCODE_BINARY
+
+import ujson
 
 argv.append("<<ssid>>")
 argv.append("<<pwd>>")
 
 
+async def onMessage(opcode, data, fin, sender, receivers):
+    if opcode == OPCODE_BINARY:
+        data = data.decode("utf-8")
+
+    if (opcode == OPCODE_TEXT or opcode == OPCODE_BINARY):
+        print(f"Data received: {data}")
+
+    for receiver in receivers:
+        await send_websocket_frame(receiver, data, opcode, fin)
+
+
+async def onConnect(receiver):
+    print(f"Connected! :)")
+    msg = {"text": "Hello Client!"}
+    await send_websocket_frame(receiver, ujson.dumps(msg), 0x1)
+
+
 async def main():
     if len(argv) == 2:
         connect(argv[0], argv[1])
-        await run_websocket_server("0.0.0.0", 8000)
+        await run_websocket_server("0.0.0.0", 8000, onConnect, onMessage)
 
 run(main())
--- projects/micropython/websocket-server/websocket.py
+++ projects/micropython/websocket-server/websocket.py
@@ -1,29 +1,22 @@
 import uhashlib as hashlib
 
 import ustruct as struct
-import base64
+import binascii
 
 import uasyncio as asyncio
 
 from http import parse_request
 
 clients = []
+handlers = {}
 
+OPCODE_TEXT = 0x1
+OPCODE_BINARY = 0x2
+OPCODE_CLOSE = 0x8
+OPCODE_PING = 0x9
+OPCODE_PONG = 0xa
 
-def create_websocket_handshake_response(key: str) -> bytes:
-    """
-    Create a WebSocket handshake response given the WebSocket key from the request
-    """
-    # The response consists of a hash of the key, followed by the magic string 258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-    print("Generating WebSocket handshake response...")
-    magic_str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-    key_bytes = str(key + magic_str).encode("utf-8")
-    key_hash = hashlib.sha1(key_bytes).digest()
-    # print(f"Key Hash: {key_hash}")
-    key_hash_b64 = base64.b64encode(key_hash).decode('utf-8')
-    # print(f"Key Hash b64: {key_hash_b64}")
-    print("Completing WebSocket handshake...")
-    return str(f"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {key_hash_b64}\r\n\r\n").encode("utf-8")
+DEBUG = 1
 
 
 def parse_websocket_request(request: dict):
@@ -86,10 +79,7 @@ async def receive_websocket_frame(reader: asyncio.StreamReader):
     #   - RSV1, RSV2, and RSV3 (1 bit each)
     #   - the Opcode (4 bits)
     b1 = int.from_bytes(await reader.readexactly(1), "big")
-    # print(f"Byte 1 - Received: {b1:b}")
-
     fin = bool(b1 & 0b10000000)
-    # print(f"FIN: {fin}")
 
     # Opcodes:
     #     `%x0` denotes a continuation frame
@@ -101,17 +91,13 @@ async def receive_websocket_frame(reader: asyncio.StreamReader):
     #     `%xA` denotes a pong
     #     `%xB`-F are reserved for further control frames
     opcode = b1 & 0b00001111
-    # print(f"Opcode: {opcode}")
 
     # The second byte of the frame is
     #     the MASK flag (1 bit)
     #     the Payload Length (7 bit)
     b2 = int.from_bytes(await reader.readexactly(1), "big")
-    # print(f"Byte 2 - Received: {b2:b}")
     mask = bool(b2 & 0b10000000)
-    # print(f"MASK: {mask}")
     payload_length = b2 & 0b01111111
-    # print(f"Payload Length: {payload_length}")
 
     # If the payload is less than 126 bytes, these two bytes encode the length directly
     if payload_length == 126:
@@ -148,45 +134,113 @@ def mask_websocket_data(masking_key: bytes, data: bytes) -> bytes:
     return masked_data
 
 
-async def handle_client(reader, writer):
-    clients.append(writer)
-    try:
-        # Perform the WebSocket handshake
-        req = await reader.read(1024)
-        request = parse_request(req)
-        key = parse_websocket_request(request)
-        print(f"Key: {key}")
-        response = create_websocket_handshake_response(key)
-        await writer.awrite(response)
-
-        # Echo messages back to the client
-        while True:
-            data, opcode, fin = await receive_websocket_frame(reader)
-            if opcode == 0x1:  # Text (utf-8)
-                print(f"Data received (Text): {data.decode('utf-8')}")
-            elif opcode == 0x2:  # Binary Data
-                print(f"Data received (Text): {data}")
-            elif opcode == 0x8:  # Connection close
-                break
-            elif opcode == 0x9:  # Ping
-                opcode = 0xa
-                print("Ping")
-            elif opcode == 0xa:  # Pong
-                print("Pong")
-            else:
-                print(f"Unhandled opcode: {opcode}")
-
-            for client in clients:
-                await send_websocket_frame(client, data, opcode, fin)
-    finally:
-        clients.remove(writer)
-        await writer.aclose()
+async def cb_connect(receiver):
+    pass
 
 
-async def run_websocket_server(host: str, port: int):
+async def cb_message(opcode, data, fin, sender, receivers):
+    pass
+
+
+def generate_response_key(webkey):
+    key = hashlib.sha1(webkey)
+    key.update(b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+    return binascii.b2a_base64(key.digest())[:-1]
+
+
+def generate_handshake_response(key):
+    # return str(f"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {key}\r\n\r\n").encode("utf-8")
+    return b"""\
+HTTP/1.1 101 Switching Protocols\r
+Upgrade: websocket\r
+Connection: Upgrade\r
+Sec-WebSocket-Accept: """ + key + b"\r\n\r\n"
+
+
+async def server_handshake(reader, writer, cb=cb_connect):
+
+    # Skip HTTP request line.
+    # req = await reader.readline()
+
+    # webkey = None
+    # upgrade = False
+    # websocket = False
+
+    # while True:
+    #     req = await reader.readline()
+    #     if not req:
+    #         return False
+    #     if req == b"\r\n":
+    #         break
+    #     header, value = [x.strip() for x in req.split(b":", 1)]
+    #     if DEBUG:
+    #         print((header, value))
+    #     if header == b"Sec-WebSocket-Key":
+    #         webkey = value
+    #     elif header == b"Connection" and b"Upgrade" in value:
+    #         upgrade = True
+    #     elif header == b"Upgrade" and value == b"websocket":
+    #         websocket = True
+
+    # if not (upgrade and websocket and webkey):
+    #     return False
+
+    # if DEBUG:
+    #     print(f"Sec-WebSocket-Key: {webkey} ({len(webkey)})")
+
+    # response_key = generate_response_key(webkey)
+    # response = generate_handshake_response(response_key)
+
+    # return True
+
+    req = await reader.read(1024)
+    request = parse_request(req)
+    key = parse_websocket_request(request)
+    response_key = generate_response_key(key)
+    response = generate_handshake_response(response_key)
+
+    await writer.awrite(response)
+    await cb(writer)
+
+
+async def process_messages(reader, writer, cb=cb_message):
+    while True:
+        print(f"Processing messages ...")
+        data, opcode, fin = await receive_websocket_frame(reader)
+        print("Message received!")
+        if opcode == OPCODE_TEXT or opcode == OPCODE_BINARY:
+            await cb(opcode, data, fin, writer, clients)
+        elif opcode == OPCODE_CLOSE:
+            break
+        elif opcode == OPCODE_PING:
+            opcode = OPCODE_PONG
+            await send_websocket_frame(writer, data, opcode, fin)
+        elif opcode == OPCODE_PONG:
+            pass
+        else:
+            print(f"Unhandled opcode: {opcode}")
+
+
+def handle_client(onConnect, onMessage):
+    async def _handle_client(reader, writer):
+        clients.append(writer)
+        try:
+            # Perform the WebSocket handshake
+            await server_handshake(reader, writer, onConnect)
+            # Process message
+            # if complete:
+            await process_messages(reader, writer, onMessage)
+        finally:
+            clients.remove(writer)
+            await writer.aclose()
+    return _handle_client
+
+
+async def run_websocket_server(host: str, port: int, onConnect, onMessage):
     """
     Run a simple WebSocket server
     """
-    server = await asyncio.start_server(handle_client, host, port)
+    # handlers[(host, port)] = handler
+    server = await asyncio.start_server(handle_client(onConnect, onMessage), host, port)
     print(f"Listening on {host}:{port}")
     await server.wait_closed()
