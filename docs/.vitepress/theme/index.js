da3428af52a6710c9a19692a0e406cf5c0820872
Alija Sabic <sabic.alija@gmail.com>
2023-02-17 13:18:21 +0100

    Fix medium-zoom handling and style

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,8 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 import Layout from "./components/Layout.vue";
-import { onMounted } from "vue";
+import { onMounted, watch, nextTick } from "vue";
+import { inBrowser, useRoute } from "vitepress";
 import mediumZoom from "medium-zoom";
 
 import "./custom.scss";
@@ -10,8 +11,12 @@ export default {
   ...DefaultTheme,
   Layout: Layout,
   setup() {
-    onMounted(() => {
-      mediumZoom(".VPDoc img", { background: "var(--vp-c-bg)" });
-    });
+    const initZoom = () => {
+      if (inBrowser) mediumZoom(".VPDoc img", { background: "var(--vp-c-bg)", margin: 100 });
+    };
+    const handler = () => nextTick(initZoom);
+    const route = useRoute();
+    onMounted(initZoom);
+    watch(() => route.path, handler);
   },
 };
